896c352af1ca6a2b2d2815f17ea5232636b25716
rudydayanapalacios-wq <rudydayanapalacios@gmail.com>
2026-09-21 15:10:37 -0500

    feat: integracion http entre cliente django y servidor fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.auth import router as auth_router
 from app.routers.gestures import router as gestures_router
@@ -8,6 +9,15 @@ app = FastAPI(
     title="Detector de Gestos API",
     description="API para detección de gestos mediante visión por computador.",
     version="1.0.0"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 
